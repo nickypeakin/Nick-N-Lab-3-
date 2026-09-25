05c5e80abfeda31924316e55e0dc72ccddea8604
nickypeakin <nnatale29@setoncatholic.org>
2026-09-25 15:43:45 +0000

    commit n sunc

---
+++ main.py
@@ -0,0 +1,35 @@
+# this function adds 2 numbers
+def add(x,y):
+    print(x+y)
+add(6,7)
+################################################
+
+#this function subtracts 2 numbers
+def subtract(x,y):
+    print(x-y)
+subtract(6,7)
+################################################
+
+# this function multiplies 2 numbers
+def multiply(x,y):
+    print(x,y)
+multiply(6,7)
+################################################
+
+# this function multiplies 2 numbers
+def divide(x,y):
+    print(x,y)
+divide(6,7)
+################################################
+
+x = int(input("enter your first number"))
+y = int(input("enter your second number"))
+
+add(x,y)
+subtract(x,y)
+multiply(x,y)
+divide(x,y)
+
+print("welcome to my awesome calc app only 69.99")
+print("What math we doing man")
+print("Type (a)dd (s)ubtract (m)ultiply (d)ivide (q)uit")
